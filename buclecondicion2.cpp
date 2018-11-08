26c4d1abc37fcafbfa9fbc9830ab8740921b30fb
Carlos Galvez <cargalvezp@gmail.com>
2018-11-08 13:59:49 +0100

    Commit Inicial

---
+++ buclecondicion2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int x=0, divisor=1, contador=0;
+	cout << "Introduce un numero para saber si es primo o no: "; cin >> x;
+	do{
+		if ( x%divisor == 0 && ){
+			contador++;
+		}
+		divisor++;
+	}
+	
+	while(divisor <= x);
+
+	if (contador == 2){
+		cout << "El numero " << x << " es Primo" << endl;	
+	}
+	else{
+		cout << "El numero " << x << " No es primo" << endl;	
+	}
+}
